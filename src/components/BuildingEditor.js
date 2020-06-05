abe421c35e7b2a6f6f56e94ccf7280ea773f9eab
kggu <miikkakortelainen1@gmail.com>
2020-06-05 09:22:26 +0300

    Update editor layout

--- src/components/BuildingEditor.js
+++ src/components/BuildingEditor.js
@@ -2,21 +2,68 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+
+import { useEditor } from "../utils/EditorProvider";
 
 const BuildingEditor = () => {
   const useStyles = makeStyles((theme) => ({
-    editor: {
+    root: {
       background: "#dddce0",
-      minHeight: "100vh",
+      minHeight: "91vh",
+    },
+    editorContainer: {
+      padding: theme.spacing(0.5),
+      minHeight: "83vh",
+    },
+    actionsContainer: {},
+    button: {
+      margin: theme.spacing(1),
     },
   }));
 
   const classes = useStyles();
 
+  const { getSteps, activeStep, nextStep, previousStep } = useEditor();
+  const steps = getSteps();
+
   return (
-    <div className={classes.editor}>
-      <Grid></Grid>
-      <Typography align="center">Designer / Area selection here</Typography>
+    <div className={classes.root}>
+      <div className={classes.editorContainer}>
+        <Paper>
+          <Typography align="center">Area selection</Typography>
+        </Paper>
+      </div>
+      <div className={classes.actionsContainer}>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          sm={12}
+          md={12}
+          lg={12}
+        >
+          <Paper>
+            <Button
+              variant="contained"
+              disabled={activeStep === 0}
+              onClick={previousStep}
+              className={classes.button}
+            >
+              Back
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={nextStep}
+              className={classes.button}
+            >
+              {activeStep === steps.length - 1 ? "Finish" : "Next"}
+            </Button>
+          </Paper>
+        </Grid>
+      </div>
     </div>
   );
 };
--- src/components/EditorStepper.js
+++ src/components/EditorStepper.js
@@ -14,28 +14,14 @@ const EditorStepper = () => {
   const useStyles = makeStyles((theme) => ({
     root: {
       width: "100%",
-      minHeight: "100vh",
-    },
-    button: {
-      marginTop: theme.spacing(1),
-      marginRight: theme.spacing(1),
-    },
-    actionsContainer: {
-      marginBottom: theme.spacing(2),
-    },
-    resetContainer: {
-      padding: theme.spacing(3),
+      minHeight: "91vh",
     },
   }));
 
   const {
     activeStep,
-    setActiveStep,
     getSteps,
     getStepContent,
-    nextStep,
-    previousStep,
-    resetSteps,
   } = useEditor();
 
   const classes = useStyles();
@@ -49,37 +35,10 @@ const EditorStepper = () => {
             <StepLabel>{label}</StepLabel>
             <StepContent>
               <Typography>{getStepContent(index)}</Typography>
-              <div className={classes.actionsContainer}>
-                <div>
-                  <Button
-                    disabled={activeStep === 0}
-                    onClick={previousStep}
-                    className={classes.button}
-                  >
-                    Back
-                  </Button>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={nextStep}
-                    className={classes.button}
-                  >
-                    {activeStep === steps.length - 1 ? "Finish" : "Next"}
-                  </Button>
-                </div>
-              </div>
             </StepContent>
           </Step>
         ))}
       </Stepper>
-      {activeStep === steps.length && (
-        <Paper square elevation={0} className={classes.resetContainer}>
-          <Typography>Completed</Typography>
-          <Button onClick={resetSteps} className={classes.button}>
-            Reset
-          </Button>
-        </Paper>
-      )}
     </div>
   );
 };
