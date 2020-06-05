f3c68e512c4b4d2115a10b6af3b1d8f0853fefe6
Sami Jouppila <sami.jouppila@gmail.com>
2020-06-05 14:45:54 +0300

    Building page in progress

--- src/sites/BuildingsPage/BuildingsPage.js
+++ src/sites/BuildingsPage/BuildingsPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import NavigationBar from '../../components/Navigation/NavigationBar';
+import Container from "@material-ui/core/Container";
 
 const BuildingsPage = () => {
-    return <NavigationBar></NavigationBar>
+    return <React.Fragment>
+        <NavigationBar />
+        <Container maxWidth="sm">
+
+        </Container>
+    </React.Fragment>
 }
 
 export default BuildingsPage;
