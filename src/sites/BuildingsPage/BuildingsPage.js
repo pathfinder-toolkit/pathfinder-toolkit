f9762780adb8f16a3d07551a8f5e0f2f24dba173
Sami Jouppila <sami.jouppila@gmail.com>
2020-06-08 08:03:49 +0300

    EnhancedTable added for BuildingsPage, editing in progress

--- src/sites/BuildingsPage/BuildingsPage.js
+++ src/sites/BuildingsPage/BuildingsPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import NavigationBar from '../../components/Navigation/NavigationBar';
 import Container from "@material-ui/core/Container";
+import BuildingsTable from "../../components/Buildings/BuildingsTable";
 
 const BuildingsPage = () => {
     return <React.Fragment>
         <NavigationBar />
         <Container maxWidth="sm">
-
+            <BuildingsTable />
         </Container>
     </React.Fragment>
 }
