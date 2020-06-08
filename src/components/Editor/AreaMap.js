a0cbaf37fa31fb838a0d5850abce54c7a711270b
kggu <miikkakortelainen1@gmail.com>
2020-06-08 09:02:06 +0300

    Split map into new component

---
+++ src/components/Editor/AreaMap.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+
+const AreaMap = (props) => {
+  const geoUrl =
+    "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+  const allowedCountries = props.allowedCountries;
+
+  const validCountryStyle = {
+    default: {
+      fill: "#F53",
+      outline: "none",
+    },
+    hover: {
+      fill: "#E42",
+      outline: "none",
+    },
+    pressed: {
+      fill: "#E42",
+      outline: "none",
+    },
+  };
+
+  const invalidCountryStyle = {
+    default: {
+      fill: "#D6D6DA",
+      outline: "none",
+    },
+    hover: {
+      fill: "#D6D6DA",
+      outline: "none",
+    },
+    pressed: {
+      fill: "#D6D6DA",
+      outline: "none",
+    },
+  };
+
+  return (
+    <ComposableMap
+      width="800"
+      height="375"
+      projection="geoAzimuthalEqualArea"
+      projectionConfig={{
+        rotate: [-20.0, -60.0, 0],
+        scale: 500,
+      }}
+    >
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies.map((geo) => {
+            if (allowedCountries.includes(geo.properties.NAME)) {
+              return (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill="#9998A3"
+                  stroke="#EAEAEC"
+                  onClick={() => props.handleSelection(geo.properties.NAME)}
+                  style={validCountryStyle}
+                />
+              );
+            } else {
+              return (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill="#9998A3"
+                  stroke="#EAEAEC"
+                  onClick={() => props.handleSelection(geo.properties.NAME)}
+                  style={invalidCountryStyle}
+                />
+              );
+            }
+          })
+        }
+      </Geographies>
+    </ComposableMap>
+  );
+};
+
+export default AreaMap;
--- src/components/Editor/AreaSelection.js
+++ src/components/Editor/AreaSelection.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography, Paper } from "@material-ui/core/";
 import { useBackend } from "../../utils/FakeBackend";
+import AreaMap from "./AreaMap";
 
 const AreaSelection = () => {
   const [selectedArea, setSelectedArea] = useState("");
@@ -21,74 +21,17 @@ const AreaSelection = () => {
   const { getCountries } = useBackend();
   const allowedCountries = getCountries();
 
+  const handleSelection = (selectedCountry) => {
+    setSelectedArea(selectedCountry);
+  };
+
   return (
     <React.Fragment>
       <Typography align="center">Select building area</Typography>
-      <ComposableMap
-        width="800"
-        height="375"
-        projection="geoAzimuthalEqualArea"
-        projectionConfig={{
-          rotate: [-20.0, -60.0, 0],
-          scale: 500,
-        }}
-      >
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map((geo) => {
-              if (allowedCountries.includes(geo.properties.NAME)) {
-                return (
-                  <Geography
-                    key={geo.rsmKey}
-                    geography={geo}
-                    fill="#9998A3"
-                    stroke="#EAEAEC"
-                    onClick={() => setSelectedArea(geo.properties.NAME)}
-                    style={{
-                      default: {
-                        fill: "#F53",
-                        outline: "none",
-                      },
-                      hover: {
-                        fill: "#E42",
-                        outline: "none",
-                      },
-                      pressed: {
-                        fill: "#E42",
-                        outline: "none",
-                      },
-                    }}
-                  />
-                );
-              } else {
-                return (
-                  <Geography
-                    key={geo.rsmKey}
-                    geography={geo}
-                    fill="#9998A3"
-                    stroke="#EAEAEC"
-                    onClick={() => setSelectedArea(geo.properties.NAME)}
-                    style={{
-                      default: {
-                        fill: "#D6D6DA",
-                        outline: "none",
-                      },
-                      hover: {
-                        fill: "#D6D6DA",
-                        outline: "none",
-                      },
-                      pressed: {
-                        fill: "#D6D6DA",
-                        outline: "none",
-                      },
-                    }}
-                  />
-                );
-              }
-            })
-          }
-        </Geographies>
-      </ComposableMap>
+      <AreaMap
+        allowedCountries={allowedCountries}
+        handleSelection={handleSelection}
+      />
       {selectedArea && (
         <div className={classes.selection}>
           <Typography>Selected country: {selectedArea}</Typography>
