61d7aea44a12bf1f9216d2852e403b7e9b315591
kggu <miikkakortelainen1@gmail.com>
2020-06-16 10:01:36 +0300

    Update NavigationBar.js

--- src/components/Navigation/NavigationBar.js
+++ src/components/Navigation/NavigationBar.js
@@ -74,7 +74,7 @@ const NavigationBar = (props) => {
         <Typography variant="h6" className={classes.title}>
           EnergyPathfinder
         </Typography>
-        <Button onClick={ () => privateRequest()}> test request</Button>
+        {//<Button onClick={ () => privateRequest()}> test request</Button>}
         <IconButton
           onClick={() => {
             redirectTo("");
