74820d910e6f523c797c630321aafffed92a2e7e
Sami Jouppila <sami.jouppila@gmail.com>
2020-06-17 14:14:00 +0300

    Sentiment selection in progress

--- src/components/Comment/CommentComponents/CommentCreationForm.js
+++ src/components/Comment/CommentComponents/CommentCreationForm.js
@@ -2,9 +2,25 @@ import React from "react";
 import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import SentimentSatisfiedAltIcon from '@material-ui/icons/SentimentSatisfiedAlt';
+import SentimentSatisfiedIcon from '@material-ui/icons/SentimentSatisfied';
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+import { green } from '@material-ui/core/colors';
 
 const CommentCreationForm = (props) => {
     const classes = props.classes;
+
+    const [value, setValue] = React.useState('female');
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
 
     return <Paper className={classes.root}>
         <TextField
@@ -18,18 +34,21 @@ const CommentCreationForm = (props) => {
         />
         <Grid container>
             <Grid item xs={3}>
-                <p>Testi</p>
+                <Typography>Sentiment:</Typography>
             </Grid>
-            <Grid item xs={3}>
-                <p>Testi</p>
-            </Grid>
-            <Grid item xs={3}>
-                <p>Testi</p>
-            </Grid>
-            <Grid item xs={3}>
-                <p>Testi</p>
+            <Grid item xs={9}>
+                <FormControl component="sentiment-form">
+                <RadioGroup row aria-label="sentiment" name="gender1" value={value} onChange={handleChange}>
+                    <FormControlLabel value="none" control={<Radio />} label="None" />
+                    <FormControlLabel value="positive" control={<Radio />} label={<SentimentSatisfiedAltIcon style={ { color: green[500]} } />} />
+                    <FormControlLabel value="neutral" control={<Radio />} label={<SentimentSatisfiedIcon />} />
+                    <FormControlLabel value="negative" control={<Radio />} label={<SentimentVeryDissatisfiedIcon style={ {fontSize: 26} } color="secondary" />}/>
+                </RadioGroup>
+        </FormControl>
             </Grid>
         </Grid>
+        
+        
     </Paper>
 }
 
