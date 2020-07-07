cb4ead3e5189e7dd1b75c32b8b41e5645de07da5
Sami Jouppila <sami.jouppila@gmail.com>
2020-07-07 13:54:26 +0300

    BuildingViewer fetches comments individually from backend

--- src/components/Comment/CommentComponents/CommentContainer.js
+++ src/components/Comment/CommentComponents/CommentContainer.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Button from '@material-ui/core/Button'
 import Paper from "@material-ui/core/Paper";
 import Switch from "@material-ui/core/Switch";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from '@material-ui/core/styles';
+
+import { useBackend } from "../../../utils/BackendProvider";
 
 import Comments from "./Comments.js";
 
@@ -32,17 +35,28 @@ const useStyles = makeStyles((theme) => ({
 const CommentContainer = (props) => {
     const classes = useStyles();
 
-    const [comments, setComments] = useState([]);
+    const [comments, setComments] = useState(null);
 
-    const [switchState, setSwitchState] = useState(false);
+    const [switchState, setSwitchState] = useState(true);
+
+    const [loading, setLoading] = useState(false);
+
+    const { requestComments } = useBackend();
     
     const _handleSwitchChange = (event) => {
         setSwitchState(switchState ? false : true);
     };
-    
+
+    const fetchComments = async () => {
+        setLoading(true);
+        const data = await requestComments(props.subject);
+        console.log(data)
+        setComments(data);
+        setLoading(false);
+    }
 
     return <Paper className={classes.root}>
-        {comments.length > 0 && (<React.Fragment>
+        {comments && (comments.length > 0) && (<React.Fragment>
                 <Switch
                 checked={switchState}
                 onChange={_handleSwitchChange}
@@ -53,12 +67,21 @@ const CommentContainer = (props) => {
                 <Typography className={classes.displayText}>{switchState ? "Hide comments" : "Show comments"}</Typography>
                 {switchState && <Comments comments={comments} classes={classes} />}
             </React.Fragment>
-         )}
-        {comments.length == 0 && (
+        )}
+        {comments && (comments.length == 0) && (
             <React.Fragment>
-            <Typography className={classes.displayText}>No comments found, add one below</Typography>
-            <Typography className={classes.displayText}>{props.subject}</Typography>
+                <Typography className={classes.displayText}>No comments found, add one below</Typography>
             </React.Fragment>
+        )}
+        {!comments && (
+            <React.Fragment>
+                {loading ? (
+                    <Typography className={classes.displayText}>Loading</Typography> 
+                ) : (
+                    <Button onClick={fetchComments}>Show comments</Button>
+                )}
+            </React.Fragment>
+
         )}
     </Paper>
 }
