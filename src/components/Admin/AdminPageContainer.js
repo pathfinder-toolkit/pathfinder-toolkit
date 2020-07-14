ca421fa1f0545aec0d980d2419868182f21c9acd
Sami Jouppila <sami.jouppila@gmail.com>
2020-07-14 10:32:46 +0300

    Admin status is verified from backend before displaying admin page

--- src/components/Admin/AdminPageContainer.js
+++ src/components/Admin/AdminPageContainer.js
@@ -3,8 +3,6 @@ import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from '@material-ui/core/styles';
 
-import AdminDashboard from "./AdminDashboard";
-
 import { useAdmin } from "../../utils/AdminProvider";
 
 const AdminPageContainer = () => {
