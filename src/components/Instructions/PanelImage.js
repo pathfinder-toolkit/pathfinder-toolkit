c3dbf584dbe0bf9447ddb7e10cfae22d351f4e83
Sami Jouppila <sami.jouppila@gmail.com>
2020-08-06 11:53:54 +0300

    Instruction panel image overflow fix

--- src/components/Instructions/PanelImage.js
+++ src/components/Instructions/PanelImage.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import CardMedia from '@material-ui/core/CardMedia'
 
 const PanelImage = ({image, style}) => {
     return <Grid item>
+        <CardMedia>
         <img src={image} className={style}></img>
+        </CardMedia>
     </Grid>
 }
 
