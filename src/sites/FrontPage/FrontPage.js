1323354735f2fa481b7dcc9712c91e493ea38d4f
kggu <miikkakortelainen1@gmail.com>
2020-09-04 12:03:29 +0300

    Add button for co-design page

--- src/sites/FrontPage/FrontPage.js
+++ src/sites/FrontPage/FrontPage.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import NavigationBar from '../../components/Navigation/NavigationBar';
+import React from "react";
+import NavigationBar from "../../components/Navigation/NavigationBar";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
-import Fab from '@material-ui/core/Fab';
-import Paper from '@material-ui/core/Paper';
-import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
-import frontPageImage from "../../external/images/frontpage_house.jpg"
-import InstructionPanels from "../../components/Instructions/InstructionPanels"
+import Fab from "@material-ui/core/Fab";
+import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import frontPageImage from "../../external/images/frontpage_house.jpg";
+import InstructionPanels from "../../components/Instructions/InstructionPanels";
 import Create from "@material-ui/icons/Create";
-import Button from "@material-ui/core/Button"
+import Button from "@material-ui/core/Button";
 
 import history from "../../utils/history";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,24 +21,25 @@ const useStyles = makeStyles((theme) => ({
   },
   banner: {
     height: 660,
-    backgroundImage: 'url(' + frontPageImage + ')',
-    backgroundPosition: 'center bottom',
+    backgroundImage: "url(" + frontPageImage + ")",
+    backgroundPosition: "center bottom",
     backgroundSize: "cover",
     display: "flex",
-    flexDirection: "column"
+    flexDirection: "column",
   },
   bannerHeader: {
     marginTop: "auto",
     marginBottom: 80,
     fontSize: "4.5em",
-    textShadow: "-0.03em 0 black, 0 0.03em black, 0.03em 0 black, 0 -0.03em black",
-    color: "white"
+    textShadow:
+      "-0.03em 0 black, 0 0.03em black, 0.03em 0 black, 0 -0.03em black",
+    color: "white",
   },
   infoGrid: {
     display: "flex",
     padding: 0,
     margin: 0,
-    flexWrap: "nowrap"
+    flexWrap: "nowrap",
   },
   infoPaper: {
     marginLeft: 10,
@@ -45,12 +47,12 @@ const useStyles = makeStyles((theme) => ({
     marginTop: 10,
     height: 100,
     display: "flex",
-    flexDirection: "column"
+    flexDirection: "column",
   },
   infoMessage: {
     margin: "auto",
     fontSize: theme.typography.pxToRem(18),
-    flexBasis: '33.33%',
+    flexBasis: "33.33%",
     flexShrink: 0,
   },
   designButton: {
@@ -62,71 +64,111 @@ const useStyles = makeStyles((theme) => ({
     "&:hover": {
       backgroundColor: "#283371",
       color: "#efe0b3",
-    }
+    },
   },
   demoButton: {
-    margin: theme.spacing(1)
-  }
+    margin: theme.spacing(1),
+  },
 }));
 
 const FrontPage = () => {
-
   const classes = useStyles();
 
   const redirectTo = (addr) => {
     history.push(addr);
-  }
+  };
 
-  return <Container maxWidth={false} className={classes.root}>
-    <NavigationBar />
+  return (
+    <Container maxWidth={false} className={classes.root}>
+      <NavigationBar />
 
-    <Grid container justify="center">
-      <Grid item xs={12} className={classes.banner}>
-        <Typography align="center" className={classes.bannerHeader} variant="h1">
-          EnergyPathfinder
+      <Grid container justify="center">
+        <Grid item xs={12} className={classes.banner}>
+          <Typography
+            align="center"
+            className={classes.bannerHeader}
+            variant="h1"
+          >
+            EnergyPathfinder
+          </Typography>
+        </Grid>
+      </Grid>
+
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        spacing={0}
+        className={classes.infoGrid}
+      >
+        <Grid item xs={4}>
+          <Paper className={classes.infoPaper}>
+            <Typography className={classes.infoMessage}>
+              Start by inputting your building details
             </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.infoPaper}>
+            <Grid container justify="center">
+              <Button
+                style={{ marginTop: "30px" }}
+                className={classes.demoButton}
+                fullWidth
+                color="primary"
+                variant="contained"
+              >
+                Co-Design
+              </Button>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.infoPaper}>
+            <Typography className={classes.infoMessage}>
+              Check out our suggestion for improvements
+            </Typography>
+          </Paper>
+        </Grid>
       </Grid>
-    </Grid>
 
-    <Grid container direction="row" alignItems="center" spacing={0} className={classes.infoGrid} >
-      <Grid item xs={4}>
-        <Paper className={classes.infoPaper}>
-          <Typography className={classes.infoMessage} >Start by inputting your building details</Typography>
-        </Paper>
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        justify="center"
+        className={classes.infoGrid}
+      >
+        <Grid item xs={4}>
+          <Paper className={classes.infoPaper}>
+            <Typography className={classes.infoMessage}>
+              View ready-made example demo buildings
+            </Typography>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.demoButton}
+              onClick={() => {
+                redirectTo("demo");
+              }}
+            >
+              Demo buildings
+            </Button>
+          </Paper>
+        </Grid>
       </Grid>
-      <Grid item xs={4}>
-        <Paper className={classes.infoPaper}>
-          <Typography className={classes.infoMessage} >Pathfinder estimates your energy efficiency</Typography>
-        </Paper>
-      </Grid>
-      <Grid item xs={4} >
-        <Paper className={classes.infoPaper}>
-          <Typography className={classes.infoMessage} >Check out our suggestion for improvements</Typography>
-        </Paper>
-      </Grid>
-    </Grid>
+      <InstructionPanels />
 
-    <Grid container direction="row" alignItems="center" justify="center" className={classes.infoGrid} >
-    <Grid item xs={4}>
-        <Paper className={classes.infoPaper}>
-          <Typography className={classes.infoMessage} >View ready-made example demo buildings</Typography>
-          <Button
-          variant="contained"
-          color="secondary"
-          className={classes.demoButton}
-          onClick={() => {redirectTo('demo')}}>
-            Demo buildings
-          </Button>
-        </Paper>
-      </Grid>
-    </Grid>
-    <InstructionPanels />
-
-    <Fab className={classes.designButton} onClick={() => { redirectTo('design') }}>
-      <Create fontSize="large" />
-    </Fab>
-
-  </Container>
-}
+      <Fab
+        className={classes.designButton}
+        onClick={() => {
+          redirectTo("design");
+        }}
+      >
+        <Create fontSize="large" />
+      </Fab>
+    </Container>
+  );
+};
 
 export default FrontPage;
